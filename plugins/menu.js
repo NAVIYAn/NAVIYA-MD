d629a4b87e9e81a88087c9cdb33bc34b66f87907
NAVIYA 🖤🎊 <nimsaranavindu927@gmail.com>
2024-09-20 21:21:23 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,6 +5,7 @@ cmd({
     pattern: "menu",
     desc: "get cmd list ",
     category: "main",
+    react: "🧚‍♂️",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
