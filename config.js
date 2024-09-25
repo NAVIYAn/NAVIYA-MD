50685fc1f5136b744fb09f4747c077803a7af3bb
NAVIYA 🖤🎊 <nimsaranavindu927@gmail.com>
2024-09-25 11:27:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,3 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "eB0yAZZT#dt_vgYNS8knu-rjSlgTTrdEIX2ktseRCB0M4UO-6cmk",
 MONGODB: process.env.MONGODB || "mongodb://mongo:fGFNiDCkljigagBHUDEIfaffkjbdlMFb@junction.proxy.rlwy.net:39122",
 };
-
-
