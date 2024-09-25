79266ce88ea9dd1c47a5c9e7524e3440c0a280c6
NAVIYA 🖤🎊 <nimsaranavindu927@gmail.com>
2024-09-25 18:59:19 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -9,8 +9,16 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, days, minutes, seconds, reply}) => {
 try{
+const uptimeMessage = `*👨‍💻ＭＡＳＴＥＲ-ＭＤ-Ｖ3 ＩＳ ＡＬＩＶＥ👨‍💻*
+*📆 Day: ${days} Day*
+*🕰️Hour: ${hours} Hour*
+*⏳ Minute: ${minutes} Minute*
+*⏲️ Second: ${seconds} Second*
+*👨‍💻Developer: MASTER MIND*
+_________________________________________
+`;
 
-const buttons = [
+  const buttons = [
         {
           "name": "quick_reply",
           "buttonParamsJson": JSON.stringify({
@@ -18,9 +26,8 @@ const buttons = [
             id: `.menu`
           })
         },
-    {
-
-    "name": "quick_reply",
+        {
+          "name": "quick_reply",
           "buttonParamsJson": JSON.stringify({
             display_text: "PING",
             id: `.ping`
@@ -28,6 +35,51 @@ const buttons = [
         }
         ];
 
+  const msg = generateWAMessageFromContent(m.from, {
+    viewOnceMessage: {
+      message: {
+        messageContextInfo: {
+          deviceListMetadata: {},
+          deviceListMetadataVersion: 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: uptimeMessage
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "© 𝐂ʀᴇᴀᴛᴇᴅ 𝐁ʏ 𝐌ʀ 𝐒ᴀʜᴀɴ 𝐎ꜰᴄ"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "",
+            gifPlayback: true,
+            subtitle: "",
+            hasMediaAttachment: false 
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons
+          }),
+          contextInfo: {
+                  mentionedJid: [m.sender], 
+                  forwardingScore: 999,
+                  isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                  newsletterJid: '120363249960769123@newsletter',
+                  newsletterName: "MASTER-MD-V3",
+                  serverMessageId: 143
+                }
+              }
+        }),
+      },
+    },
+  }, {});
+
+  await Matrix.relayMessage(msg.key.remoteJid, msg.message, {
+    messageId: msg.key.id
+  });
+    }
+};
+
+export default alive;
 
 
 
