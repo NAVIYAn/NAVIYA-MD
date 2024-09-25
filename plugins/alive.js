5b2eb6cb207ee6dac663a8355f6f37746eec7d8f
NAVIYA 🖤🎊 <nimsaranavindu927@gmail.com>
2024-09-25 18:49:16 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -9,8 +9,16 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, days, minutes, seconds, reply}) => {
 try{
-const config = await readEnv();
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+
+const buttons = [
+        {
+          "name": "quick_reply",
+          "buttonParamsJson": JSON.stringify({
+            display_text: "MENU",
+            id: `.menu`
+          })
+        },
+    {
 }catch(e){
 console.log(e)
 reply(`${e}`)
