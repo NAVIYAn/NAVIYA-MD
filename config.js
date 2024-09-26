4d81ea8073fbf4cbfd0287dbec48e995cf1043ab
NAVIYA 🖤🎊 <nimsaranavindu927@gmail.com>
2024-09-26 05:52:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "eB0yAZZT#dt_vgYNS8knu-rjSlgTTrdEIX2ktseRCB0M4UO-6cmk",
+SESSION_ID: process.env.SESSION_ID || "taFBkRAT#-pje8o-_85YSUHxfNxOJmkIeiVZbayZ176O-dYj5xFE",
 MONGODB: process.env.MONGODB || "mongodb://mongo:fGFNiDCkljigagBHUDEIfaffkjbdlMFb@junction.proxy.rlwy.net:39122",
 };
 
