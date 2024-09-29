75466cafc0a0812f19db2d4adccc321b406f8313
NAVIYA 🖤🎊 <nimsaranavindu927@gmail.com>
2024-09-29 10:22:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Vz8xRI6L#dv-ZHK-8FDLDvW0zdWgvyh49OELwTYplvWLyLxhQ9iM",
 MONGODB: process.env.MONGODB || "mongodb://mongo:ReMsMhOSBwDQqIwIKpXbHRanJcJiYNmZ@autorack.proxy.rlwy.net:43688",
-OMDB_API_KEY:process.env.OMDB_API_KEY || "http://www.omdbapi.com/?i=tt3896198&apikey=db6731e5",
+OMDB_API_KEY:process.env.OMDB_API_KEY || "http://www.omdbapi.com/?i=tt3896198&apikey=db6731e5"
 };
 
