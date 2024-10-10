20a684db06d91f1b9f3e5416d2ba46d598941b5b
NAVIYA 🖤🎊 <nimsaranavindu927@gmail.com>
2024-10-10 16:29:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aAlWFDoK#IH1Sjz9pAc16dJqQkHCfLzymg2KXWk3WHsBRgcCNYT4",
+SESSION_ID: process.env.SESSION_ID || "UjsnGDpC#O41zxn9p98yXXs92LgbRmvPzs_vXZdae6M3mFUbj7oU",
 MONGODB: process.env.MONGODB || "mongodb://mongo:nCNtvmpZQfbmQyteQXzHtYWyzpjMSpcp@autorack.proxy.rlwy.net:55349",
 OMDB_API_KEY:process.env.OMDB_API_KEY || "http://www.omdbapi.com/?i=tt3896198&apikey=db6731e5" 
 };
